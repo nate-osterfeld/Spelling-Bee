9447361d6e754f60c903dbc4d004e89654a59763
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2025-02-08 18:05:44 -0500

    Save user to database

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys.js')
+const pool = require('../db.js')
 
 passport.use(
 	new GoogleStrategy(
@@ -9,8 +10,28 @@ passport.use(
 			clientSecret: keys.GOOGLE_CLIENT_SECRET,
 			callbackURL: '/auth/google/callback',
 		},
-        (accessToken, refreshToken, profile, done) => {
-            console.log(profile)       
+        async (accessToken, refreshToken, profile, done) => {
+            // google info
+            const values = [profile.id, profile.displayName, profile.emails[0].value]
+
+            // select/insert queries
+            const query_selectUser = 'SELECT * FROM users WHERE google_id = $1'
+            const query_insertUser = 'INSERT INTO users (google_id, name, email) VALUES ($1, $2, $3)'
+
+            // select user
+            let userResults = await pool.query(query_selectUser, [profile.id])
+            
+            // if user exists -> done(user)
+            if (userResults.rows.length) {
+                const user = userResults.rows[0]
+                done(null, user)
+            // else insert user -> select user -> done(user)
+            } else {
+                await pool.query(query_insertUser, values)
+                userResults = await pool.query(query_selectUser, [profile.id])
+                const user = userResults.rows[0]
+                done(null, user)
+            }
         }
 	)
 )
