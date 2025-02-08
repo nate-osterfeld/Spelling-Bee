206c4f5beb04c654372f0c11467fa6df13f16d79
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2025-02-08 17:03:29 -0500

    Google OAuth flow

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config
 const express = require('express')
+const passport = require('passport')
+require('./services/passport.js')
 const q = require('./queries')
 
 const app = express()
@@ -7,13 +9,22 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    console.log('GET /')
-    return res.json('This route works')
+	console.log('GET /')
+	return res.json('This route works')
 })
 
 app.get('/users', q.getUsers)
 
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email'],
+	}),
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log('Express server listening on port: ', PORT)
+	console.log('Express server listening on port: ', PORT)
 })
