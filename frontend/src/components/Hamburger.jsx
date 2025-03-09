b6433c4e7b3d2404b8aab520f95386ef012e3ca2
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-03-09 14:28:16 -0400

    Create email form

--- frontend/src/components/Hamburger.jsx
+++ frontend/src/components/Hamburger.jsx
@@ -1,10 +1,15 @@
 import './Hamburger.css'
 
-function HamburgerMenu({ setShowDropdown, showDropdown }) {
+function toggleMenus(setShowDropdown, showDropdown, setShowSignup, showSignup) {
+	setShowDropdown(!showDropdown)
+	setShowSignup(false)
+}
+
+function HamburgerMenu({ setShowDropdown, showDropdown, setShowSignup, showSignup }) {
     return (
         <>
             <input type='checkbox' id='checkbox' />
-            <label for='checkbox' className='toggle' onClick={() => setShowDropdown(!showDropdown)}>
+            <label for='checkbox' className='toggle' onClick={() => toggleMenus(setShowDropdown, showDropdown, setShowSignup, showSignup)}>
                 <div class='bars' id='bar1'></div>
                 <div class='bars' id='bar2'></div>
                 <div class='bars' id='bar3'></div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import './Navbar.css'
 import google_icon from '../assets/google-icon.svg'
 import email_icon from '../assets/email-icon.svg'
+import back_icon from '../assets/back-square-icon.svg'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import HamburgerMenu from './Hamburger'
 
 function Navbar() {
 	const [showDropdown, setShowDropdown] = useState(false)
+	const [showSignup, setShowSignup] = useState(false)
 
 	return (
 		<>
@@ -15,19 +17,27 @@ function Navbar() {
 					<img className='home__bee-logo' src='/assets/bee-logo.png' alt='logo' />
 				</Link>
 				<div className='hamburger-menu'>
-					<HamburgerMenu setShowDropdown={setShowDropdown} showDropdown={showDropdown} />
+					<HamburgerMenu
+						setShowDropdown={setShowDropdown}
+						showDropdown={showDropdown}
+						setShowSignup={setShowSignup}
+						showSignup={showSignup}
+					/>
 				</div>
 			</nav>
 			{showDropdown && (
 				<div className='signup-container'>
 					<div className='signup'>
 						<div className='signup__header'>Join SpeeBee!</div>
-						<a href='/auth/google' className='signup__method'>
+						<a
+							href={`${import.meta.env.VITE_BACKEND_URL}/auth/google`}
+							className='signup__method'
+						>
 							<img src={google_icon} alt='Google Logo' />
 							Sign in with Google
 							<div></div>
 						</a>
-						<button href='/auth/google' className='signup__method'>
+						<button className='signup__method' onClick={() => setShowSignup(true)}>
 							<img src={email_icon} alt='Google Logo' />
 							Sign up with Email
 							<div></div>
@@ -46,6 +56,42 @@ function Navbar() {
 								Privacy Policy
 							</a>{' '}
 							applies to you.
+						</p>
+					</div>
+				</div>
+			)}
+
+			{showSignup && (
+				<div className='signup-container'>
+					<div className='signup'>
+						<button className='signup__back' onClick={() => setShowSignup(false)}>
+							<img src={back_icon} alt="Back Icon" />
+						</button>
+						<div className='signup__header2'>Sign up with email</div>
+						<p className='signup__ptext'>Enter your email address and password to create an account.</p>
+						<form>
+							<label htmlFor=''>Email</label>
+							<input className='signup__input' type='text' required />
+
+							<label htmlFor=''>Password</label>
+							<input className='signup__input' type='password' required />
+
+							<label htmlFor=''>Password</label>
+							<input className='signup__input' type='password' required />
+
+							<button className='signup__submit' type='submit'>Submit</button>
+						</form>
+
+						<p className='signup__terms'>
+							This site is protected by reCAPTCHA Enterprise and the{' '}
+							<a href='https://policies.google.com/privacy' rel='noopener follow' target='_blank'>
+								Google Privacy Policy
+							</a>{' '}
+							and{' '}
+							<a href='https://policies.google.com/terms' rel='noopener follow' target='_blank'>
+								Terms of Service
+							</a>{' '}
+							apply.
 						</p>
 					</div>
 				</div>
