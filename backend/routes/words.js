3c2801d1ffaf9707071694dc2fda7c5308dc705c
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-03-29 17:45:01 -0400

    Route handler to save user word history

--- backend/routes/words.js
+++ backend/routes/words.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const pool = require('../db.js')
+const utils = require('../lib/utils.js')
 
 router.get('/', async (req, res) => {
     const { level } = req.query
@@ -15,6 +16,26 @@ router.get('/', async (req, res) => {
     let { rows } = await pool.query(query_getRandomWord)
     
     res.status(200).json(rows[0])
+})
+
+// Save word to user's history
+router.post('/save', utils.authMiddleware, async (req, res) => {
+    const { isCorrect, word } = req.body
+    
+    if (req.user) {
+        const query_saveToHistory =
+            'INSERT INTO wordshistory (created_at, is_correct, user_id, word_id) VALUES(' +
+            'NOW(), $1, $2, (SELECT id FROM words WHERE word = $3))'
+        let { rowCount } = await pool.query(query_saveToHistory, [isCorrect, req.user.id, word])
+    
+        if (rowCount) {
+            return res.status(200).json({ success: true, message: 'Successfully saved word to user history' })
+        }
+    
+        return res.json({ success: false, message: 'Unable to save word to user history' })
+    }
+
+    return res.json({ message: 'Please sign in to save words to your history' })
 })
 
 module.exports = router
