fd913b8e1682903fc73709a27eac53595fbec71d
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-04-05 00:14:38 -0400

    Add production level environment variables

---
+++ backend/config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+	cookie_key: process.env.COOKIE_KEY,
+}
--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-})
+const pool =
+	process.env.NODE_ENV === 'production'
+		? new Pool({
+				connectionString: process.env.DATABASE_URL,
+				ssl: {
+					rejectUnauthorized: false, // Render requires SSL
+				},
+		  })
+		: new Pool({
+				user: process.env.DB_USER,
+				host: process.env.DB_HOST,
+				database: process.env.DB_NAME,
+				password: process.env.DB_PASSWORD,
+				port: process.env.DB_PORT,
+		  })
 
 pool.connect()
-    .then(() => console.log("Connected to Postgres database"))
-    .catch((err) => console.log("Error connecting to Postgres database", err))
+	.then(() => console.log(`Connected to Postgres database: ${process.env.NODE_ENV}`))
+	.catch((err) => console.log('Error connecting to Postgres database', err))
 
 module.exports = pool
